4c1089b52736716ae212ab2b3ccd9e1c729e51e8
Maurício Oliveira <manutencaomauricio81@gmail.com>
2025-02-16 14:41:36 -0300

    first commit

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const pool = new Pool({
-    user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: process.env.DB_SSL === 'true', // Habilita SSL se necessário
+  });
 
 // Rota de login
 app.post('/login', async (req, res) => {
